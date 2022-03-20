cca4d17dc5c90640cc8064ac77e776987ea55b95
hatrala <31583739+hatrala@users.noreply.github.com>
2022-03-20 17:54:44 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express  = require('express')
-const Router = express.Router()
+const router = express.Router()
 
 const AuthController = require('../controllers/AuthController')
 const router = require('./employee')
