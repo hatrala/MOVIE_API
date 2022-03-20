d91f8887760a331589420c0234379c4d399f4cbb
hatrala <31583739+hatrala@users.noreply.github.com>
2022-03-20 18:43:47 +0700

    Update authenticate.js

--- middlewave/authenticate.js
+++ middlewave/authenticate.js
@@ -4,7 +4,7 @@ const { route } = require('../routes/auth')
 
 const authenticate = (req, res, next)=>{
     try{
-        const token = req.headers.authorization.split('')[1]
+        const token = req.headers.authorization.split(' ')[1]
         const decode = jwt.verify(token, 'abcdefg')
 
         req.user = decode
