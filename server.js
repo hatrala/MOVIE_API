b6d5ab2ac937d1b980d375500d9ff2f52aac48d9
hatrala <31583739+hatrala@users.noreply.github.com>
2022-04-23 18:36:22 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () =>{
     console.log('Sever is running on port', {PORT})
